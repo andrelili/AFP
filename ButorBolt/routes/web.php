b2ecf0ff91d60d48494bb14c431d6635db7d386a
André Lili Anna <166949390+andrelili@users.noreply.github.com>
2025-10-08 19:24:19 +0200

    RegisterController létrehozása

--- ButorBolt/routes/web.php
+++ ButorBolt/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
-Route::get('/', function () {
-    return view('register');
+Route::get('/', function() {
+    return redirect('/register');
 });
+
+Route::get('/register', [RegisterController::class, 'show']);
