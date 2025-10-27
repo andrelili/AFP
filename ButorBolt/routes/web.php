223ba36a93f5b889a8da8d2c282405c3535bfedf
André Lili Anna <166949390+andrelili@users.noreply.github.com>
2025-10-27 15:51:31 +0100

    FavoritesController létrehozása

--- ButorBolt/routes/web.php
+++ ButorBolt/routes/web.php
@@ -6,12 +6,14 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\BagCheckoutController;
 use App\Http\Controllers\SuccessfulOrderController;
+use App\Http\Controllers\FavoritesController;
 
 Route::get('/', [HomeController::class, 'show'])->name('home');
 Route::match(['get', 'post'], '/register', [RegisterController::class, 'register'])->name('register');
 Route::get('/bag-checkout', [BagCheckoutController::class, 'show'])->name('bag.checkout');
 Route::get('/items/{id}', [App\Http\Controllers\ItemController::class, 'show'])->name('items.show');
 Route::get('/successful-order', [SuccessfulOrderController::class, 'show'])->name('successful.order');
+Route::get('/favorites', [FavoritesController::class, 'show'])->name('favorites.show');
 
 Route::get('/', [HomeController::class, 'show'])->name('home');
 Route::get('/items/{id}', [ItemController::class, 'show'])->name('items.show');
